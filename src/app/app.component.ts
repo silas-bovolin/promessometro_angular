3d6532889b7477d25b4551e94e60911636609609
Silas <silas.reis.bovolin@gmail.com>
2025-07-02 16:15:51 -0300

    tela de loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,30 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
-import { filter } from 'rxjs';
+import { filter, Observable } from 'rxjs';
+import { LoadingComponent } from './components/loading/loading.component';
+import { LoadingService } from './services/loading/loading.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, NavbarComponent, CommonModule],
+  imports: [RouterModule, NavbarComponent, CommonModule, LoadingComponent],
   templateUrl: `./app.component.html`,
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
   title = 'promessometro_angular';
   showNavbar: boolean = false;
-  constructor(private router: Router) {
+  loading$: Observable<boolean>;
+
+  constructor(
+    private router: Router,
+    private loadingService: LoadingService
+  ) {
     this.router.events.pipe(
       filter((e): e is NavigationEnd => e instanceof NavigationEnd))
       .subscribe((ev: NavigationEnd) => {
         this.showNavbar = !ev.url.endsWith('/');
       });
-
+    this.loading$ = this.loadingService.loading$;
   }
 }
---
+++ src/app/interceptors/loading/loading.interceptor.ts
@@ -0,0 +1,12 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { LoadingService } from '../../services/loading/loading.service';
+import { inject } from '@angular/core';
+import { finalize } from 'rxjs';
+
+export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+  const loadingService = inject(LoadingService);
+  loadingService.show();
+  return next(req).pipe(
+    finalize(() => loadingService.hide())
+  );
+};
